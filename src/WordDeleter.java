80a21de21b0b6a8cba7ec60a21b9985cf9f21790
JuliaM88 <julia.moskalenko@outlook.com>
2021-12-29 17:12:48 +0100

    init

--- src/WordDeleter.java
+++ src/WordDeleter.java
@@ -1,28 +1,31 @@
 class WordDeleter {
     public String remove(String phrase, String[] words) {
-        String[] phrases;
-        phrases = phrase.split(" ");
-        String s = "";
-                   for (String w: words){
-                       for (String value : phrases){
-                                 if (!value.equals(w)) {
-                                    s = "" + value;
+
+        String s = " " + phrase + " ";
+        String k = "";
+
+        for (String w: words){
+
+            String m = " " + w;
+            s = s.replace(m, "") ;
+        }
 
 
-                                         // System.out.println(s);
-                                      }
-                             }
-                   }
+        // System.out.println(s);
+
+
         return s.trim();
     }
-}
 
+}
 class WordDeleterTest {
     public static void main(String[] args) {
         WordDeleter wordDeleter = new WordDeleter();
 
-        //lutoekj mynopybi
-        System.out.println(wordDeleter.remove("vys ycly zawofu lutoekj mynopybi egces", new String[] {"vys", "zawofu", "ycly", "egces"}));
+
+
+        //matajilyh objiqono
+        System.out.println(wordDeleter.remove("potou vys matajilyh objiqono zawofu egces", new String[] {"zawofu", "egces", "potou", "vys" }));
 
         //This Sparta
         System.out.println(wordDeleter.remove("This is Sparta", new String[] {"is"}));
